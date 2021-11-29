5efe5e039aeb049f669544cf120c25e43e49ebd3
Шкловский Андрей Васильевич <ashklovskiy@phoenixit.ru>
2021-11-29 07:26:25 +0300

    added detection of combined calls and links to child calls

--- lib/fs_log_parser/call.rb
+++ lib/fs_log_parser/call.rb
@@ -2,10 +2,23 @@ module FsLogParser
   class Call
     def initialize(**params)
       @params = params
+      @start_time = params.delete(:start_time)
       @lines = []
+      @call_length = 0
+      @links = []
     end
 
-    attr_reader :params
-    attr_accessor :lines
+    def end_time=(timestamp)
+      @end_time = timestamp
+      @call_length = @end_time - @start_time
+
+      @end_time
+    end
+
+    def links_objects
+      @links_objects ||= @links.map { |l| Calls.find(l) }
+    end
+
+    attr_reader :params, :end_time, :call_length, :links, :lines
   end
 end
--- lib/fs_log_parser/parser.rb
+++ lib/fs_log_parser/parser.rb
@@ -4,33 +4,49 @@ module FsLogParser
       @calls = Calls.new
     end
 
-    def parse_file(filename)
+    def parse_file(filename, call_uuid = nil)
       file = File.open(filename, 'r')
-      file.each_with_index do |line, index|
-        begin
-          parse(line)
-        rescue => e
-          # puts "#{e.message} : #{line}"
-        end
-      end
+      file.each { |line| parse_line(line, filename) }
     end
   
-    def parse(line)
+    private
+    def parse_line(line, filename)
       line = line.encode("UTF-8", invalid: :replace, replace: "")
       line_uuid = get_line_uuid(line)
       call = Calls.find_in_processing(line_uuid)
   
-      if line =~ /New\sChannel/
+      case line
+      when /New\sChannel/
         start_time = Time.parse(parse_time(line))
         call_uuid = get_call_uuid(line)
-        call = Call.new(line: line, start_time: start_time, uuid: call_uuid)
+        caller = get_caller(line)
+        caller_domain = caller.split('@').last
+        call = Call.new(start_line: line, 
+                        start_time: start_time, 
+                        uuid: call_uuid, 
+                        caller: caller,
+                        caller_domain: caller_domain,
+                        filename: filename)
         call.lines << line
         @calls << call
-      elsif line =~ /State\sDESTROY\sgoing\sto\ssleep/
+      when /Dialplan.+Absolute\sCondition/
+        return if call.nil?
+
+        route = get_route(line)
+        call.params[:resulting_route] = route
+      when /State\sDESTROY\sgoing\sto\ssleep/
+        return if call.nil?
+
         end_time = Time.parse(parse_time(line))
-        call.params[:end_time] = end_time
+        call.end_time = end_time
         call.lines << line
         @calls.finish(call)
+      when /CombineCalls2Connection/
+        return if call.nil?
+
+        links = line.match(/\((.*)\)/)&.send(:[], 1).split.values_at(1,3)
+        links.delete(line_uuid)
+        call.links << links.first
       else
         return if call.nil?
   
@@ -52,6 +68,10 @@ module FsLogParser
   
     def get_route(line)
       line.match(/\[(.*)\]/)&.send(:[], 1).to_s
+    end
+
+    def get_caller(line)
+      line.split[-2]
     end
   end
 end
